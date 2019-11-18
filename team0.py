f8b84cac7f296c90d86a42e9339a83898a91f70b
Seth-KandyKane <57458708+Seth-KandyKane@users.noreply.github.com>
2019-11-18 14:24:48 -0500

    Add files via upload

---
+++ team0.py
@@ -0,0 +1,7 @@
+import random
+def mySolution(history):
+     number = random.randint(1,2)
+     if number == 2:
+          return "betray the sinner"              #choose to defect
+     else:
+          return "Cooperate with the POPO"              #choose to cooperate
